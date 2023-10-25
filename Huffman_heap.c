5c32f6529dedc8587e4f0753ecb652ea6acc762f
mrtnkrlv <martink011204@gmail.com>
2023-10-25 13:01:17 +0200

    beginning project

---
+++ Huffman_heap.c
@@ -0,0 +1,21 @@
+struct huffmanHeap{
+    char symbol;
+    int weight;
+    struct huffmanHeap* left_child;
+    struct huffmanHeap* right_child;
+};
+typedef struct huffmanHeap biheap;
+
+/*biheap heap_creation(){
+    return {.symbol = }
+}*/
+
+int main(){
+
+    return 0;
+}
+
+
+
+
+
